8d8b0d837f15be1c513a043052e095e85ebdee04
dkode8 <dkode8@8c3eb3c4-eb3a-0410-862c-73fa8ce6028b>
2008-12-17 03:43:01 +0000

    creating branch to tinker with the api to make it easier to support more db's

---
+++ branches/api.changes/src/Migrator.Tests/Providers/OracleTransformationProviderTest.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Configuration;
+using Migrator.Providers.Oracle;
+using Migrator.Tests.Providers;
+using NUnit.Framework;
+
+namespace Migrator.Tests.Providers
+{
+    [TestFixture, Category("Oracle")]
+    public class OracleTransformationProviderTest : TransformationProviderConstraintBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            string constr = ConfigurationManager.AppSettings["OracleConnectionString"];
+            if (constr == null)
+                throw new ArgumentNullException("OracleConnectionString", "No config file");
+            _provider = new OracleTransformationProvider(new OracleDialect(), constr);
+            _provider.BeginTransaction();
+
+            AddDefaultTable();
+        }
+    }
+}
---
+++ branches/api.changes/src/Migrator.Tests/Providers/PostgreSQLTransformationProviderTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Configuration;
+using Migrator.Providers.PostgreSQL;
+using Migrator.Tests.Providers;
+using NUnit.Framework;
+
+namespace Migrator.Tests.Providers
+{
+    [TestFixture, Category("Postgre")]
+    public class PostgreSQLTransformationProviderTest : TransformationProviderConstraintBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            string constr = ConfigurationManager.AppSettings["NpgsqlConnectionString"];
+            if (constr == null)
+                throw new ArgumentNullException("ConnectionString", "No config file");
+
+            _provider = new PostgreSQLTransformationProvider(new PostgreSQLDialect(), constr);
+            _provider.BeginTransaction();
+            
+            AddDefaultTable();
+        }
+    }
+}
